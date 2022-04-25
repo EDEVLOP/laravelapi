83b14d891effd21e890f36c28aa01db99d798a08
abinash889 <abinash889@gmail.com>
2022-04-25 10:32:24 +0530

    initialization

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Validator;
+
+class UserController extends Controller
+{
+   
+    public function users() {
+        $result=User::all(['id as user_id','name as fullname','email']);
+        $tres=count($result);
+        // foreach($result as $res){
+           
+        //     $data[]=$res->email;
+        // }
+        return response()->json([
+            'status'=>200,
+            
+            'users'=>$result
+            
+        ],200);
+
+        
+    }
+}
